707c40737ebe8c2eb323e11433472fc9c24c9293
matthew-nichols <matthew.nichols@mx.com>
2020-07-14 12:49:03 -0600

    Upgrade dependencies and add jacoco

--- src/main/groovy/com/mx/coppuccino/CoppuccinoPlugin.groovy
+++ src/main/groovy/com/mx/coppuccino/CoppuccinoPlugin.groovy
@@ -6,6 +6,7 @@ import groovy.transform.TypeCheckingMode
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPlugin
+import org.kordamp.gradle.plugin.jacoco.JacocoPlugin
 import ru.vyarus.gradle.plugin.quality.QualityPlugin
 import io.gitlab.arturbosch.detekt.DetektPlugin
 
@@ -14,6 +15,8 @@ class CoppuccinoPlugin implements Plugin<Project> {
 
   @CompileStatic(TypeCheckingMode.SKIP)
   void apply(Project project) {
+    def extension = project.extensions.create('coppuccino', CoppuccinnoPluginExtension)
+
     project.plugins.withType(JavaPlugin) {
       project.afterEvaluate {
         def initCopConfig = new CopyCopConfig()
@@ -25,6 +28,7 @@ class CoppuccinoPlugin implements Plugin<Project> {
         project.plugins.apply(QualityPlugin)
         project.plugins.apply(SpotlessPlugin)
         project.plugins.apply(DetektPlugin)
+        project.plugins.apply(JacocoPlugin)
         project.configure(project) {
           quality {
             checkstyleVersion = '8.29'
@@ -37,7 +41,6 @@ class CoppuccinoPlugin implements Plugin<Project> {
           }
           spotless {
             groovy {
-              paddedCell()
               greclipse('2.3.0').configFile('.coppuccino/spotless/eclipse-formatter.xml')
               target project.fileTree(project.rootDir) {
                 include '**/*.gradle', '**/*.groovy'
@@ -45,7 +48,6 @@ class CoppuccinoPlugin implements Plugin<Project> {
               }
             }
             java {
-              paddedCell()
               importOrder 'java', 'javax', 'edu', 'com', 'org', 'brave', 'io', 'reactor'
               // A sequence of package names
               eclipse().configFile '.coppuccino/spotless/eclipse-formatter.xml'
@@ -59,12 +61,41 @@ class CoppuccinoPlugin implements Plugin<Project> {
             kotlin {
               ktlint()
             }
-
           }
           detekt {
             config = files(".coppuccino/detekt/detekt.yml")
             excludes: ".*build.*,.*/resources/.*,.*/tmp/.*"
 
+          }
+
+          //making sure coverage report is generated in the test case call
+          test.finalizedBy jacocoTestReport
+
+          //adding Jacoco test coverage to be done in check command
+          check.dependsOn jacocoTestCoverageVerification
+
+          jacocoTestReport {
+            reports {
+              csv.enabled true
+              html.enabled true
+            }
+            executionData(test)
+          }
+
+          jacocoTestCoverageVerification {
+            violationRules {
+              rule {
+                limit {
+                  value = 'COVEREDRATIO'
+                  minimum = extension.minimumCoverage
+                }
+                excludes = [
+                    'com.mx.mdx.models.*',
+                    'com.mx.mdx.Resources.*',
+                    'com.mx.mdx.Resources'
+                ]
+              }
+            }
           }
 
         }
