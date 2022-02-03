322a888bef1c801d56fdbb506d64c7f6a35d288a
Robert Hanna <robert.hanna@mx.com>
2022-02-03 09:55:39 -0700

    Fix bug where coppuccino config doesn't get copied

--- src/main/groovy/com/mx/coppuccino/CopyCopConfig.groovy
+++ src/main/groovy/com/mx/coppuccino/CopyCopConfig.groovy
@@ -10,31 +10,31 @@ class CopyCopConfig {
   }
 
   void run() {
-    File target = new File(Paths.get("./.coppuccino/pmd/pmd.xml").toAbsolutePath().toString())
+    File target = new File(Paths.get(".coppuccino/pmd/pmd.xml").toString())
     target.getParentFile().mkdirs()
     InputStream stream = getClass().getResourceAsStream("/com/mx/coppuccino/config/pmd/pmd.xml")
     target.text = ''
     target << stream.text
 
-    target = new File(Paths.get("./.coppuccino/checkstyle/checkstyle.xml").toAbsolutePath().toString())
+    target = new File(Paths.get(".coppuccino/checkstyle/checkstyle.xml").toString())
     target.getParentFile().mkdirs()
     stream = getClass().getResourceAsStream("/com/mx/coppuccino/config/checkstyle/checkstyle.xml")
     target.text = ''
     target << stream.text
 
-    target = new File(Paths.get("./.coppuccino/spotbugs/exclude.xml").toAbsolutePath().toString())
+    target = new File(Paths.get(".coppuccino/spotbugs/exclude.xml").toString())
     target.getParentFile().mkdirs()
     stream = getClass().getResourceAsStream("/com/mx/coppuccino/config/spotbugs/exclude.xml")
     target.text = ''
     target << stream.text
 
-    target = new File(Paths.get("./.coppuccino/spotless/eclipse-formatter.xml").toAbsolutePath().toString())
+    target = new File(Paths.get(".coppuccino/spotless/eclipse-formatter.xml").toString())
     target.getParentFile().mkdirs()
     stream = getClass().getResourceAsStream("/com/mx/coppuccino/config/spotless/eclipse-formatter.xml")
     target.text = ''
     target << stream.text
 
-    target = new File(Paths.get("./.coppuccino/detekt/detekt.yml").toAbsolutePath().toString())
+    target = new File(Paths.get(".coppuccino/detekt/detekt.yml").toString())
     target.getParentFile().mkdirs()
     stream = getClass().getResourceAsStream("/com/mx/coppuccino/config/detekt/detekt.yml")
     target.text = ''
